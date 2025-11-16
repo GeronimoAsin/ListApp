6612ec3182e7b0a8b195f7a66530fb523588d576
Geronimo Asin <163333828+GeronimoAsin@users.noreply.github.com>
2025-11-16 13:04:55 -0300

    nav bar cambia a vertical en landscape mode

--- app/src/main/java/ar/edu/itba/listapp/ui/composables/BottomNavBar.kt
+++ app/src/main/java/ar/edu/itba/listapp/ui/composables/BottomNavBar.kt
@@ -1,14 +1,28 @@
 package ar.edu.itba.listapp.ui.composables
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.NavigationRailItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteDefaults
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import ar.edu.itba.listapp.ui.layouts.AppDestination
@@ -22,38 +36,84 @@ fun AdaptiveNavBar(
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+    val configuration = LocalConfiguration.current
 
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestination.entries.forEach { destination ->
-                item(
-                    icon = {
-                        Icon(
-                            imageVector = destination.icon,
-                            contentDescription = stringResource(destination.label)
-                        )
-                    },
-                    label = { Text(stringResource(destination.label)) },
-                    selected = currentRoute == destination.route,
-                    onClick = {
-                        navController.navigate(destination.route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
+    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        Row(modifier = modifier) {
+            NavigationRail(
+                containerColor = DeeperGreen,
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+                        AppDestination.entries.forEach { destination ->
+                            NavigationRailItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = destination.icon,
+                                        contentDescription = stringResource(destination.label)
+                                    )
+                                },
+                                label = { Text(stringResource(destination.label)) },
+                                selected = currentRoute == destination.route,
+                                onClick = {
+                                    navController.navigate(destination.route) {
+                                        popUpTo(navController.graph.startDestinationId)
+                                        launchSingleTop = true
+                                    }
+                                },
+                                colors = NavigationRailItemDefaults.colors(
+                                    indicatorColor = MaterialTheme.colorScheme.secondaryContainer,
+                                    selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                                    selectedTextColor = MaterialTheme.colorScheme.onSecondaryContainer
+                                )
+                            )
                         }
                     }
-                )
+                }
             }
-        },
-        navigationSuiteColors = NavigationSuiteDefaults.colors(
-            navigationBarContainerColor = DeeperGreen,
-            navigationBarContentColor = Color.White,
-            navigationRailContainerColor = DeeperGreen,
-            navigationRailContentColor = Color.White,
-            navigationDrawerContainerColor = DeeperGreen,
-            navigationDrawerContentColor = Color.White
-        ),
-        modifier = modifier
-    ) {
-        content()
+            Box(Modifier.weight(1f)) {
+                content()
+            }
+        }
+    } else {
+        Scaffold(
+            modifier = modifier,
+            bottomBar = {
+                NavigationBar(
+                    containerColor = DeeperGreen,
+                ) {
+                    AppDestination.entries.forEach { destination ->
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    imageVector = destination.icon,
+                                    contentDescription = stringResource(destination.label)
+                                )
+                            },
+                            label = { Text(stringResource(destination.label)) },
+                            selected = currentRoute == destination.route,
+                            onClick = {
+                                navController.navigate(destination.route) {
+                                    popUpTo(navController.graph.startDestinationId)
+                                    launchSingleTop = true
+                                }
+                            },
+                            colors = NavigationBarItemDefaults.colors(
+                                indicatorColor = MaterialTheme.colorScheme.secondaryContainer,
+                                selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                                selectedTextColor = MaterialTheme.colorScheme.onSecondaryContainer
+                            )
+                        )
+                    }
+                }
+            }
+        ) { paddingValues ->
+            Box(modifier = Modifier.padding(paddingValues)) {
+                content()
+            }
+        }
     }
 }
