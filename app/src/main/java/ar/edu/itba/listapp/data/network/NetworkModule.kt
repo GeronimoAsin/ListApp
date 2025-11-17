95c9bc99c83b232db655eae867e1a78df4d552b0
Geronimo Asin <163333828+GeronimoAsin@users.noreply.github.com>
2025-11-17 11:44:38 -0300

    Fix creacion de listas

--- app/src/main/java/ar/edu/itba/listapp/data/network/NetworkModule.kt
+++ app/src/main/java/ar/edu/itba/listapp/data/network/NetworkModule.kt
@@ -27,6 +27,7 @@ object NetworkModule {
     private val json = Json {
         ignoreUnknownKeys = true
         coerceInputValues = true
+        encodeDefaults = true
     }
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
